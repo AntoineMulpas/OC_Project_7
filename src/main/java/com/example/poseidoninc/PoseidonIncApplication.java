5d3741ec4f53b11f1526be8b4fd7fc89860b0bf5
AntoineMulpas <mulpas.antoine@gmail.com>
2023-03-10 10:04:44 +0100

    Initial commit

---
+++ src/main/java/com/example/poseidoninc/PoseidonIncApplication.java
@@ -0,0 +1,13 @@
+package com.example.poseidoninc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoseidonIncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoseidonIncApplication.class, args);
+	}
+
+}
