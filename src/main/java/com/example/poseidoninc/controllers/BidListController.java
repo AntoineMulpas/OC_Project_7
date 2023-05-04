f21bce0333859838650fc9e8e07b8c0e50f79f55
AntoineMulpas <mulpas.antoine@gmail.com>
2023-05-04 10:47:53 +0200

    Implementing logging.

--- src/main/java/com/example/poseidoninc/controllers/BidListController.java
+++ src/main/java/com/example/poseidoninc/controllers/BidListController.java
@@ -3,6 +3,8 @@ package com.example.poseidoninc.controllers;
 import com.example.poseidoninc.domain.Bid;
 import com.example.poseidoninc.services.BidListService;
 import jakarta.validation.Valid;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -18,6 +20,9 @@ public class BidListController {
 
     private final BidListService bidListService;
 
+    private static final Logger logger = LogManager.getLogger(BidListController.class);
+
+
     @Autowired
     public BidListController(BidListService bidListService) {
         this.bidListService = bidListService;
@@ -26,6 +31,7 @@ public class BidListController {
     @GetMapping("/bid/list")
     public String home(Model model, Authentication authentication)
     {
+        logger.info(authentication.getName() + " fetches all bids.");
         model.addAttribute("bidList", bidListService.findAllBids());
         // TODO: call service find all bids to show to the view
         boolean admin = authentication.getAuthorities().contains(new SimpleGrantedAuthority("ADMIN"));
@@ -34,41 +40,50 @@ public class BidListController {
     }
 
     @GetMapping("/bid/add")
-    public String addBidForm(Bid bid) {
+    public String addBidForm(Bid bid, Authentication authentication) {
+        logger.info(authentication.getName() + " fetches page to add bid.");
         return "bidList/add";
     }
 
     @PostMapping("/bid/validate")
-    public String validate(@Valid @ModelAttribute("bid") Bid bid, BindingResult result, Model model) {
+    public String validate(@Valid @ModelAttribute("bid") Bid bid, BindingResult result, Model model, Authentication authentication) {
         // TODO: check data valid and save to db, after saving return bid list
         if (result.hasErrors()) {
+            logger.error("Bid is not valid for user " + authentication.getName());
             return "bidList/add";
         }
         bidListService.saveNewBid(bid);
+        logger.info(authentication.getName() + " added a new bid." );
         return "redirect:/bid/list";
     }
 
     @GetMapping("/bidList/update/{id}")
-    public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
+    public String showUpdateForm(@PathVariable("id") Integer id, Model model, Authentication authentication) {
         Bid bidById = bidListService.getBidById(id);
         model.addAttribute("bidToBeUpdated", bidById);
-        // TODO: get Bid by Id and to model then show to the form
+        logger.info(authentication.getName() + " fetches page to update bid.");
         return "bidList/update";
     }
 
     @PostMapping("/bidList/update/{id}")
     public String updateBid(@PathVariable("id") Integer id, @Valid @ModelAttribute("bid") Bid bid,
-                             BindingResult result, Model model) {
+                             BindingResult result, Model model, Authentication authentication) {
         // TODO: check required fields, if valid call service to update Bid and return list Bid
+        if (result.hasErrors()) {
+            logger.error("Bid is not valid for user when trying to update: " + authentication.getName());
+            return "bidList/update";
+        }
         bidListService.updateBid(id, bid);
+        logger.info(authentication.getName() + " update a bid." );
         return "redirect:/bid/list";
     }
 
     @GetMapping("/bidList/delete/{id}")
-    public String deleteBid(@PathVariable("id") Integer id, Model model) {
+    public String deleteBid(@PathVariable("id") Integer id, Model model, Authentication authentication) {
         // TODO: Find Bid by Id and delete the bid, return to Bid list
         bidListService.deleteBid(id);
         model.addAttribute("bidList", bidListService.findAllBids());
+        logger.info(authentication.getName() + " has deleted bid.");
         return "redirect:/bid/list";
     }
 }
